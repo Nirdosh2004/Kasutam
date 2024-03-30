3427b3d3f49f3584170c3b89f964f7edf616393b
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-03-30 20:23:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const scroll = new LocomotiveScroll({
+          el: document.querySelector('.main'),
+          smooth: true
+      });
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/2.6.1/browser/overlayscrollbars.browser.es6.min.js" integrity="sha512-48eUZ9NudvASJ8GfcUVVjSHtR+7L5R22m5/WipgfwYceuGh0oaGddD2mY4Btl42dNw4x6rKH2zrgwi9lIshUBg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
